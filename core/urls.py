0ec97f400ea8af391f79c8f600b06e642d9474e5
kofi-teddy <agudogot@gmail.com>
2023-01-09 12:11:14 +0000

    testing login for oauth 2

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import user_list
+from core.views import user_list, login_v2
 admin.autodiscover()
 
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('users/', user_list, name="user-list"),
+    path('login/', login_v2, name="login-v2"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import Group, User
+from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework import permissions
 
 from core.serializer import UserSerializer
-from rest_framework.decorators import api_view, permission_classes
 
 
 @api_view(["GET"])
@@ -20,3 +24,24 @@ def user_list(request):
         "message": "Successful",
         "data": serializer.data
     })
+
+
+
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def login_v2(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'},
+                        status=status.HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key},
+                    status=status.HTTP_200_OK)
+
